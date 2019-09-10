afff15c269ab0857da5ed6b1ec7f6f992223267a
may <yi.ma@alphainsight.ai>
2019-09-10 16:27:06 +0800

    可运行版本

---
+++ com-server-eureka/src/main/java/com/mayi/comservereureka/IndexController.java
@@ -0,0 +1,19 @@
+package com.mayi.comservereureka;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname com-server-discover
+ * @Author: Tony
+ * @Description:
+ * @Date: Create in 16:15 2019/9/10
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping("index")
+    public String index(){
+        return "Success";
+    }
+}
