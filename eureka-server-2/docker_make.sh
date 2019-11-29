9c8645ae91ae2547ec2ea58ed4e18f9e759a7d4f
may <yi.ma@alphainsight.ai>
2019-11-29 17:11:02 +0800

    eurekadev环境更新

--- eureka-server-2/docker_make.sh
+++ eureka-server-2/docker_make.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-docker build -t registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:latest .
-docker push registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:latest
+sudo docker build -t registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:latest .
+sudo docker push registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:latest
 image_tag=`date +%Y%m%d` #_%H%M
 echo "当前时间：$image_tag"
-docker tag registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:latest registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:v${image_tag}
-docker push registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:v${image_tag}
-docker rmi registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:v${image_tag}
+sudo docker tag registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:latest registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:v${image_tag}
+sudo docker push registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:v${image_tag}
+sudo docker rmi registry.cn-shanghai.aliyuncs.com/list_of_eureka/service_eureka_2:v${image_tag}
